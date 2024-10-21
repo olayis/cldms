943fb7adc15e010e0160c8f7dbf6668f3be00bfc
olayis <olayinkasegunsolo@gmail.com>
2024-10-21 02:40:34 +0100

    chore: rename project from cldms-e2e to frontend-e2e in cypress configuration

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('cldms-e2e', () => {
+describe('frontend-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
